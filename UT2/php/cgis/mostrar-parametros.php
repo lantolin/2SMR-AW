6af7813cb91546d5282aaa2c4aea46757de1363d
lantolin <luis.antolin@gmail.com>
2021-12-12 18:17:01 +0100

    cgi PHP para visualizar los parámetros de un GET, para usarlo con los formularios

---
+++ UT2/php/cgis/mostrar-parametros.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Parametros de la request (GET)</title>
+  <meta charset="utf-8">
+</head>
+
+<body>
+ <h1>Parametros de la request (GET)</h1>
+ <ul>
+<?php
+ foreach ($_GET as $clave => $valor) {
+     echo "<li>Nombre: <strong>$clave</strong> => Valor: <strong>$valor</strong></li>";
+ }
+?>
+ </ul>
+<body>
+</html>
+
